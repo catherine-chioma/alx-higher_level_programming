08a6178ceb1248c763bda042bad00f5215b09a44
catherine-chioma <chi.ihuaku@gmail.com>
2023-03-13 06:56:00 -0700

    python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alphabet in range(26):
-if alphabet != 4 and alphabet != 16:
-print("{:s}".format(chr(alphabet + ord("a"))), end="")
+for char in range(26):
+    if char != 4 and char != 16:
+        print("{:s}".format(chr(char + ord("a"))), end="")
